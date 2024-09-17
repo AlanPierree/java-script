f4d3238d5939d9b2662dfe3ec21eb5329992e726
AlanPierree <161715146+AlanPierree@users.noreply.github.com>
2024-09-17 08:52:35 -0300

    Add files via upload

--- js/principal.js
+++ js/principal.js
@@ -1,5 +1,27 @@
 var titulo = document.querySelector(".titulo");
-
+titulo.textContent = "Aparecida Nutricionista";
 // .textContent seleciona apenas o conteúdo textual de tags como p e h1
 
-titulo.textContent = "Aparecida Nutrição"
+// hashtag indica um id, ponto indica uma classe
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = document.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+console.log(paciente); // tr
+console.log(tdPeso); // td que tem o peso
+console.log(peso); // conteúdo sem o td
+
+var imc = peso / (altura * altura);
+// IMC = 100 / (2.00 x 2.00)
+// IMC = 100 / 4.00
+// IMC = 25
+
+var tdImc = paciente.querySelector(".info-imc");
+if (peso <= 0) {
+    console.log("Peso inválido");
+}
+tdImc.textContent = imc;
