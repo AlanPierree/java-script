9bd1cac758c37bde32fccdd64a326111bd12ad8a
AlanPierree <161715146+AlanPierree@users.noreply.github.com>
2024-09-22 19:08:37 -0300

    Add files via upload

--- js/principal.js
+++ js/principal.js
@@ -55,14 +55,41 @@ titulo.addEventListener("click", function () {
 
 // addEventListener reage a interações do usuário
 // function pode ser declarada e nomeada separadamente, ou anonimamente no mesmo ()
+// event.preventDefaullt evita um comportamneto padrão
 
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click", function(event){
     event.preventDefault();
-    console.log("Botão foi clicado.");
 
     var form = document.querySelector("#form-adiciona");
-    var nome = form.nome;
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr");
+    
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    // .createElement cria um novo elemento no código;
+    // .appendChild denomina elementos como filho de outros
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr)
 })
 
-// event.preventDefaullt evita um comportamneto padrão
